574f65b3fac418fb6625d5dc219daf7c02feba45
JokerOnWeed <anthony.vasta@outlook.fr>
2025-05-27 17:52:30 +0200

    MàJ du style de l'application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,7 @@ import { Router } from '@angular/router';
 import { AuthService } from './shared/auth.service';
 import { LoginComponent } from './login/login.component';
 
+
 @Component({
   selector: 'app-root',
   imports: [
